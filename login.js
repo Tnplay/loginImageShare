02e28747aae0e20f7a238c8f1201efcf5edd474a
Tnplay <tnplayyoutube@yahoo.com.br>
2017-12-25 21:19:16 -0200

    Create login.js

---
+++ login.js
@@ -0,0 +1,8 @@
+function check(form) { 
+                if(form.userid.value == "admin" && form.pswrd.value == "12345") {
+                    alert("Senha Correta")
+                }
+                else {
+                    alert("Error Password or Username")
+                }
+            }
